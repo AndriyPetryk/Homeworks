a5a4202a5abad2f7cf5533cf50386fd2beef5a5d
Andriy <js2566531@gmail.com>
2022-12-12 18:15:16 +0200

    Homework 1

---
+++ 01/HW1.py
@@ -0,0 +1,35 @@
+# TASK 1
+worlds = ('I Love Python')
+
+print(worlds)
+
+worlds *= 42
+
+print(worlds)
+
+#TASK 2,3
+age_in_month = (12*22)
+print(age_in_month)
+
+age_in_years = (age_in_month//12)
+print(age_in_years)
+
+#TASK 4
+my_age = ('My name is Andriy, Im 22 years old')
+print(my_age)
+
+#TASK 5
+num = 1
+
+print(num == 5)
+print(num >= -1)
+print(num <= 3)
+print(num != 5)
+print(num == 1)
+
+#TASK 6
+a = 2
+b = 5
+c = 6
+d = str(a) + str(b) + str(c)
+print(d)
