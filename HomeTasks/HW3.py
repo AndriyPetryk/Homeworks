d401f7a757c562c3693b23061e5a9d64224080d8
Andriy <js2566531@gmail.com>
2022-12-20 14:33:14 +0200

    Homework_fix

--- HomeTasks/HW3.py
+++ HomeTasks/HW3.py
@@ -1,14 +1,14 @@
 #TASK 1
-# text = input('Enter the text: ')
-#
-# for letter in text:
-#     if letter.isdigit():
-#         if int(letter) % 2 == 0:
-#             print(f"{letter} even numer")
-#         else:
-#             print(f"{letter} not even numer")
-#     else:
-#         print(f"{letter}")
+text = input('Enter the text: ')
+
+for letter in text:
+    if letter.isdigit():
+        if int(letter) % 2 == 0:
+            print(f"{letter} even numer")
+        else:
+            print(f"{letter} not even numer")
+    else:
+        print(f"{letter}")
 
 
 # TASK 2
